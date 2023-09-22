1092558c74c09132410ed9582e44aa4f9d1ce982
Om Gore <omgore4307@gmail.com>
2023-09-23 02:35:58 +0530

    FrontEnd-Backend

--- Backend/server.py
+++ Backend/server.py
@@ -1,29 +1,79 @@
-from flask import Flask, jsonify
-from flask_pymongo import PyMongo
+from flask import Flask, jsonify, send_file, request
+from flask_cors import CORS
 from extract_audio import extract_video_and_audio
 import threading
 
 app = Flask(__name__)
-
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/video_database'
-mongo = PyMongo(app)
-
-
+CORS(app)
+# hindi, bengali, tamil, telugu, gujarati
 # FrontEnd to Cloudinary
-@app.route("/upload", methods=["POST"])
-def upload_video():
+@app.route("/upload/<language>", methods=["POST"])
+def upload_video(language):
     try:
-        folder_name = "Raw Files/Video"
-        public_id = f"{folder_name}/vid"
-
-        # extract_video_and_audio("vid1.mp4")
-        # Start a new thread to execute the extract_video_and_audio function
-        thread = threading.Thread(target=extract_video_and_audio, args=("vid1.mp4",))
+        video = "vid1.mp4"
+        lang = language
+        thread = threading.Thread(target=extract_video_and_audio, args=(video, lang))
         thread.start()
 
         return jsonify({"message": "Video received successfully. Processing has started"})
 
     except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/subtitle", methods=["GET"])
+def get_subtitle():
+    try:
+        subtitle_path = "Output\\eng_sub.txt"
+
+        return send_file(subtitle_path)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/trans_sub", methods=["GET"])
+def get_trans_sub():
+    try:
+        eng_subtitle = "Output\\trans_sub.txt"
+
+        return send_file(eng_subtitle)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/audio", methods=["GET"])
+def get_audio():
+    try:
+        audioFile = "extracted\\audio_only.mp3"
+
+        return send_file(audioFile)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/trans_audio", methods=["GET"])
+def get_trans_audio():
+    try:
+        transAudioFile = "Output\\translated_audio.mp3"
+
+        return send_file(transAudioFile)
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/dub", methods=["GET"])
+def get_dubbed_video():
+    try:
+        dubbed_video = "Final\\dubbed_video.mp4"
+
+        return send_file(dubbed_video)
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
         return jsonify({"error": str(e)}), 500
 
 
