ad6903b491cc4245c620d869ad9fe1026fb14f59
Om Gore <omgore4307@gmail.com>
2023-09-23 10:07:47 +0530

    Upload Remaining, Everything Working

--- Frontend/video-dubbing/src/components/OptionCard.jsx
+++ Frontend/video-dubbing/src/components/OptionCard.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import playButton from "../assests/playLogo.png";
-import "./Opton.css"; 
+import "./Opton.css";
 import axios from "axios";
+import PropTypes from "prop-types";
 
 const OptionCard = (props) => {
   const [selectedLanguage, setSelectedLanguage] = useState("hin");
-  const [showDrawer, setShowDrawer] = useState(false); // State to control drawer visibility
 
   const languageOptions = [
     { value: "hin", label: "Hindi" },
     { value: "mar", label: "Marathi" },
-    { value: "guj", label: "Gujarathi" },
-    { value: "kan", label: "Kanada" },
+    { value: "guj", label: "Gujarati" },
+    { value: "kan", label: "Kannada" },
     { value: "tel", label: "Telugu" },
   ];
 
@@ -19,48 +19,69 @@ const OptionCard = (props) => {
     setSelectedLanguage(e.target.value);
   };
 
-  const addPart = () => {
-    // "Add This Part" button is clicked.
-    console.log(`Part added in ${selectedLanguage}`);
-    // Set showDrawer to true to display the drawer
-    setShowDrawer(true);
-  };
 
-  const closeDrawer = () => {
-    // Close the drawer by setting showDrawer to false
-    setShowDrawer(false);
-  };
-
-  const downloadAudio = async () => {
+  const downloadFile = async (url, fileName, fileType) => {
     try {
-      // Make a GET request to fetch the audio file
-      const response = await axios.get(`http://localhost:5000/audio`, {
-        responseType: "blob", // Specify response type as blob
-        params: {
-          language: selectedLanguage,
-        },
-      });
+      const response = await axios.get(url, { responseType: "blob" });
 
       if (response.status === 200) {
-        // Handle the audio file, e.g., you can play it or initiate a download
-        const blob = new Blob([response.data], { type: "audio/mpeg" });
-        const url = window.URL.createObjectURL(blob);
+        const blob = new Blob([response.data], { type: fileType });
+        const fileUrl = window.URL.createObjectURL(blob);
         const a = document.createElement("a");
-        a.href = url;
-        a.download = "audio.mp3";
+        a.href = fileUrl;
+        a.download = fileName;
         a.click();
-        window.URL.revokeObjectURL(url);
+        window.URL.revokeObjectURL(fileUrl);
       } else {
-        alert("Failed to download audio.");
+        alert(`Failed to download ${fileName}.`);
       }
     } catch (error) {
-      console.error("Error downloading audio:", error);
+      console.error(`Error downloading ${fileName}:`, error);
     }
+  };
+
+  const handleDownload = () => {
+    switch (props.title) {
+      case "Video to Audio":
+        downloadFile(
+          `http://localhost:5000/audio?language=${selectedLanguage}`,
+          "audio.mp3",
+          "audio/mpeg"
+        );
+        break;
+      case "Video Subtitles":
+        downloadFile(
+          `http://localhost:5000/subtitle`,
+          "English Subtitles.txt",
+          "text/plain"
+        );
+        break;
+      case "Translated Subtitles":
+        downloadFile(
+          `http://localhost:5000/trans_sub`,
+          "Translated Subtitles.txt",
+          "text/plain"
+        );
+        break;
+      case "Dub a Video":
+        downloadFile(
+          `http://localhost:5000/dub`,
+          "dubbed_video.mp4",
+          "video/mp4"
+        );
+        break;
+      default:
+        alert("Invalid option.");
+    }
+  };
+
+  OptionCard.propTypes = {
+    title: PropTypes.string.isRequired, 
   };
 
   return (
     <div className="flex justify-center">
-      <div className="boxStyle w-[767px] h-[361px] justify-center relative"> {/* Increased height */}
+      <div className="boxStyle w-[767px] h-[361px] justify-center relative">
         <img className="w-[73px] h-[57px] rounded-[5px] m-5" src={playButton} />
         <h1 className="cardText p-10">{props.title}</h1>
         <select
@@ -75,25 +96,14 @@ const OptionCard = (props) => {
           ))}
         </select>
 
-        <button onClick={addPart} className="buttonStyle">
+        <button onClick={handleDownload} className="buttonStyle">
           Download
         </button>
 
-        {/* Conditionally render the drawer */}
-        {showDrawer && (
-          <div className="drawer">
-            <button onClick={closeDrawer} className="closeButton">
-              Close Drawer
-            </button>
-            <h2>Audio Download Options</h2>
-            <button onClick={downloadAudio} className="downloadButton">
-              Download Audio
-            </button>
-          </div>
-        )}
       </div>
     </div>
   );
+  
 };
 
 export default OptionCard;
